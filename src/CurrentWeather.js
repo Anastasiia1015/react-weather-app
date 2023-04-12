d1dec2c650631ce8c85562717636a31d8d1f8287
Anastasiia Peliukhovska <119167044+Anastasiia1015@users.noreply.github.com>
2023-04-12 22:53:55 +0200

    trying to make icons work

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,13 +1,38 @@
 import React, {useState, useEffect} from "react";
 import axios  from "axios";
-import tempPic from "./images/unknown.png";
 import { RotatingLines } from 'react-loader-spinner';
-import DateInfo from "./DateInfo";
-
+import DateInfo from "./DateInfo";  
+import IMAGES from "./images/images.js";
+                                                        
 
 
 export default function CurrentWeather({ city }) {
-  const [weatherData, setWeatheData] = useState({})
+  const iconMapping = {
+    "01d" : "dsun",
+    "01n" : "nsun",
+    "02d" : "dsuncloud",
+    "02n" : "nsuncloud",
+    "03d" : "dcloudsun",
+    "03n" : "ncloudsun",
+    "04d" : "dcloud",
+    "04n" : "ncloud",
+    "09d" : "drain",
+    "09n" : "nrain",
+    "10d" : "dshower",
+    "10n" : "nshower",
+    "13d" : "dsnow",
+    "13n" : "nsnow",
+    "50d" : "dfog",
+    "50n" : "nfog"
+  }
+  const [weatherData, setWeatheData] = useState({
+    temperature: null,
+    humidity: null,
+    description: null,
+    wind: null,
+    icon: null,
+    timezone: null,})
+  const [weatherPicUrl, setWeatherPicUrl] = useState(`./images/unknown.png`)
   useEffect(() => {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bd3bb6534458ba51b48c49f5155745b6&units=metric`;
     axios
@@ -21,17 +46,19 @@ export default function CurrentWeather({ city }) {
             icon: response.data.weather[0].icon,
             timezone:response.data.timezone,
         });
+        setWeatherPicUrl(`../src/images/${response.data.weather[0].icon}.png`)
       })
       .catch((error) => {
         console.error("Error fetching weather data:", error);
       });
   }, [city]);
+  
   if(weatherData.temperature){
     return (
       <div className="CurrentWeather">
       <div className="WeatherInfo col">
       <div className="row">
-        <img src={tempPic} alt={weatherData.description} />
+        <img src={IMAGES[iconMapping[weatherData.icon]]} alt={weatherData.description} />
         <div className="col">
         <p className="temperature">
         <span id="degrees">{weatherData.temperature}</span>
@@ -65,8 +92,8 @@ export default function CurrentWeather({ city }) {
     <RotatingLines
     strokeColor="lightblue"
     strokeWidth="5"
-    animationDuration="3"
-    width="96"
+    animationDuration="0.1"
+    width="200"
     visible={true}
     />
   </div>
