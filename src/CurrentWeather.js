bf125361588174a9aed5fc239428b1434b64b8ec
Anastasiia Peliukhovska <119167044+Anastasiia1015@users.noreply.github.com>
2023-04-12 23:17:10 +0200

    added working icons

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -2,28 +2,43 @@ import React, {useState, useEffect} from "react";
 import axios  from "axios";
 import { RotatingLines } from 'react-loader-spinner';
 import DateInfo from "./DateInfo";  
-import IMAGES from "./images/images.js";
+import dsun from "./images/01d.png"
+import nsun from "./images/01n.png"
+import dsuncloud from "./images/02n.png"
+import nsuncloud from "./images/02d.png"
+import dcloudsun from "./images/03d.png"
+import ncloudsun from "./images/03n.png"
+import dcloud from "./images/04d.png"
+import ncloud from "./images/04n.png"
+import drain from "./images/09d.png"
+import nrain from "./images/09n.png"
+import dshower from "./images/10d.png"
+import nshower from "./images/10n.png"
+import dsnow from "./images/13d.png"
+import nsnow from "./images/13n.png"
+import dfog from "./images/50d.png"
+import nfog from "./images/50n.png"
                                                         
 
 
 export default function CurrentWeather({ city }) {
   const iconMapping = {
-    "01d" : "dsun",
-    "01n" : "nsun",
-    "02d" : "dsuncloud",
-    "02n" : "nsuncloud",
-    "03d" : "dcloudsun",
-    "03n" : "ncloudsun",
-    "04d" : "dcloud",
-    "04n" : "ncloud",
-    "09d" : "drain",
-    "09n" : "nrain",
-    "10d" : "dshower",
-    "10n" : "nshower",
-    "13d" : "dsnow",
-    "13n" : "nsnow",
-    "50d" : "dfog",
-    "50n" : "nfog"
+    "01d" : dsun,
+    "01n" : nsun,
+    "02d" : dsuncloud,
+    "02n" : nsuncloud,
+    "03d" : dcloudsun,
+    "03n" : ncloudsun,
+    "04d" : dcloud,
+    "04n" : ncloud,
+    "09d" : drain,
+    "09n" : nrain,
+    "10d" : dshower,
+    "10n" : nshower,
+    "13d" : dsnow,
+    "13n" : nsnow,
+    "50d" : dfog,
+    "50n" : nfog
   }
   const [weatherData, setWeatheData] = useState({
     temperature: null,
@@ -58,7 +73,7 @@ export default function CurrentWeather({ city }) {
       <div className="CurrentWeather">
       <div className="WeatherInfo col">
       <div className="row">
-        <img src={IMAGES[iconMapping[weatherData.icon]]} alt={weatherData.description} />
+        <img src={iconMapping[weatherData.icon]} alt={weatherData.description} />
         <div className="col">
         <p className="temperature">
         <span id="degrees">{weatherData.temperature}</span>
