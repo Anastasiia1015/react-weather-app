2aed13d6d1a36f76446ffed8232038dcddc1b5e0
Anastasiia Peliukhovska <119167044+Anastasiia1015@users.noreply.github.com>
2023-04-14 20:25:30 +0200

    build issue fixed

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,4 @@
 import React from "react";
-import IMAGES  from "./images/images";
 import dsun from "./images/01d.png"
 import nsun from "./images/01n.png"
 import dsuncloud from "./images/02n.png"
